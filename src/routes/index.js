53038329de4ea10bc443c86778e6a58db091a254
bimantoro <muhbimantoro08@gmail.com>
2026-03-16 20:10:18 +0800

    feat: documents

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import users from '../service/users/routes/index.js';
-import authentications from '../service/authentications/routes/index.js';
+import users from '../services/users/routes/index.js';
+import authentications from '../services/authentications/routes/index.js';
+import documents from '../services/documents/routes/index.js';
 
 const router = Router();
 
 router.use('/auth', authentications);
 router.use('/users', users);
+router.use('/documents', documents);
 
 export default router;
--- src/services/documents/routes/index.js
+++ src/services/documents/routes/index.js
@@ -1,1 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
+import authenticationToken from '../../../middlewares/auth.js';
+import { getAll as getDocs, getById as getDocById } from '../controller/document-controller.js';
+
+const router = Router();
+
+router.get('/', authenticationToken, getDocs);
+router.get('/:id', authenticationToken, getDocById);
+
+export default router;
