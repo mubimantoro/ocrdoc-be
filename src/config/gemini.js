ffb218dd447a3eea3966545a2c35f718dbf2c867
bimantoro <muhbimantoro08@gmail.com>
2026-03-17 15:55:07 +0800

    fix: model gemini

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
 });
 
-export const CHEAP_MODEL  = process.env.GEMINI_CHEAP_MODEL  || 'gemini-2.0-flash';
-export const SMART_MODEL  = process.env.GEMINI_SMART_MODEL  || 'gemini-2.5-pro-preview-05-06';
+export const CHEAP_MODEL  = process.env.GEMINI_CHEAP_MODEL;
+export const SMART_MODEL  = process.env.GEMINI_SMART_MODEL;
 
 export default ai;
