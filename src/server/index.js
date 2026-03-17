a062bb070d5e79329be27dc13b9b72f77d63afe1
bimantoro <muhbimantoro08@gmail.com>
2026-03-17 17:08:02 +0800

    feat: swagger doc

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 import ErrorHandler from '../middlewares/error.js';
 import cors from 'cors';
 import routes from '../routes/index.js';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import yaml from 'js-yaml';
+
+const swaggerDoc = yaml.load(readFileSync('./swagger.yaml', 'utf-8'));
 
 const app = express();
 app.use(express.json());
@@ -10,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: '*'
 }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api', routes);
 app.use(ErrorHandler);
 
