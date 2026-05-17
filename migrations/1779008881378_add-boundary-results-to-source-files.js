229229f69345e32a4f8d77dc516bcbcb1a605aec
iki-rumondor <ikirumondor07@gmail.com>
2026-05-17 17:10:10 +0800

    feat: add migration for boundary

---
+++ migrations/1779008881378_add-boundary-results-to-source-files.js
@@ -0,0 +1,21 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE source_files 
+    ADD COLUMN IF NOT EXISTS boundary_results JSONB;
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE source_files 
+    DROP COLUMN IF EXISTS boundary_results;
+  `);
+};
